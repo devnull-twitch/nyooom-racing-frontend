1bbb53a2073a97408f25e982a94d51b09a24666f
devnull-twitch <devnull-twitch@devnull-twitch>
2023-02-04 20:30:25 +0100

    wip

--- src/TeamRow.tsx
+++ src/TeamRow.tsx
@@ -1,58 +1,58 @@
-import React, { FC } from 'react';
-import { Avatar, Grid, Stack, TableCell, TableRow, Typography } from "@mui/material";
+import React, { FC, useState } from 'react';
+import { Avatar, Grid, Stack, Table, TableCell, TableRow, Typography } from "@mui/material";
 import { red } from '@mui/material/colors';
-
-interface DriverData {
-  name: string;
-  points: number;
-  image: string;
-}
-
-interface TeamData {
-  name: string;
-  points: number;
-  image: string;
-  drivers: DriverData[];
-};
+import { Team } from './data/interfaces';
 
 interface TeamRowProps {
   pos: number;
-  team: TeamData;
+  team: Team;
 };
 
 export const TeamRow: FC<TeamRowProps> = ({ pos, team }) => {
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
+  const onTeamRowSelect = () => setShowDetails(!showDetails);
+
   return (
-    <TableRow>
-      <TableCell>#{pos}</TableCell>
-      <TableCell sx={{ borderRight: ' 1px solid gray' }}>
-        <Grid container justifyContent="flex-start">
-          <Stack direction="row" spacing={2}>
-            <Avatar src={team.image} sx={{ bgcolor: red[50] }}>
-              {team.name.substring(0, 1).toUpperCase()}
-            </Avatar>
-            <Stack>
-              <Typography variant="teamname">{team.name}</Typography>
-              <Typography variant="tinypoints">{team.points} points</Typography>
-            </Stack>
-          </Stack>
-        </Grid>
-      </TableCell>
-      {team.drivers.map(d => (
-        <TableCell>
-          <Grid container justifyContent="center">
+    <>
+      <TableRow onClick={onTeamRowSelect}>
+        <TableCell>#{pos}</TableCell>
+        <TableCell sx={{ borderRight: ' 1px solid gray' }}>
+          <Grid container justifyContent="flex-start">
             <Stack direction="row" spacing={2}>
-              <Avatar src={d.image} sx={{ bgcolor: red[50] }}>
-                {d.name.substring(0, 1).toUpperCase()}
+              <Avatar src={team.image} sx={{ bgcolor: red[50] }}>
+                {team.name.substring(0, 1).toUpperCase()}
               </Avatar>
               <Stack>
-                <Typography variant="username">{d.name}</Typography>
-                <Typography variant="tinypoints">{d.points} points</Typography>
+                <Typography variant="teamname">{team.name}</Typography>
+                <Typography variant="tinypoints">{team.points || 0} points</Typography>
               </Stack>
             </Stack>
           </Grid>
         </TableCell>
+        {team.drivers.map(d => (
+          <TableCell key={d.name}>
+            <Grid container justifyContent="center">
+              <Stack direction="row" spacing={2}>
+                <Avatar src={d.image} sx={{ bgcolor: red[50] }}>
+                  {d.name.substring(0, 1).toUpperCase()}
+                </Avatar>
+                <Stack>
+                  <Typography variant="username">{d.name}</Typography>
+                  <Typography variant="tinypoints">{d.points || 0} points</Typography>
+                </Stack>
+              </Stack>
+            </Grid>
+          </TableCell>
+        ))}
+      </TableRow>
+      {showDetails && team.results && team.results.map((r) => (
+        <TableRow>
+          <TableCell colSpan={2}>{r.event_name}</TableCell>
+          <TableCell colSpan={2}>{r.driver} Scored {r.points} points coming in at position {r.position}</TableCell>
+        </TableRow>
       ))}
-    </TableRow>
+    </>
   );
 };
 
--- src/data/create.ts
+++ src/data/create.ts
@@ -1,54 +1,25 @@
 import { open, readdir } from 'node:fs/promises';
 import { join } from 'node:path';
-
-interface DriverResult {
-    position: number;
-    points: number;
-}
-
-interface Driver {
-    image: string;
-    name: string;
-    points: number;
-    results: DriverResult[];
-}
-
-interface TeamResult {
-    driver: string;
-    position: number;
-    points: number;
-}
-
-interface Team {
-    image: string;
-    name: string;
-    points: number;
-    drivers: Driver[];
-    results: TeamResult[];
-}
-
-interface Teams {
-    teams:  Team[];
-}
-
-interface Race {
-    name: string;
-    give_points: boolean;
-    standings: string[];
-}
+import { Race, Teams } from "./interfaces";
 
 const teamFileHandle = await open('src/data/teams.json');
 const teamFileStr = await teamFileHandle.readFile({encoding: 'utf-8'});
 const teamsData = JSON.parse(teamFileStr) as Teams;
 
 const raceResultFiles = await readdir('src/data/races');
+raceResultFiles.sort();
+
 for (const f of raceResultFiles) {
     const fh = await open(join('src/data/races', f));
     const buf = await fh.readFile({encoding: 'utf-8'});
     const raceResultData = JSON.parse(buf) as Race;
 
     for (const [index, driverName] of raceResultData.standings.entries()) {
-        const points = raceResultData.standings.length - index;
+        let points = 0;
+        if (raceResultData.give_points) {
+            points = raceResultData.standings.length - index;
+        }
+        
         const matchingTeamIndex = teamsData.teams.findIndex((t) => t.drivers.some((d) => d.name === driverName));
         if (matchingTeamIndex !== -1) {
             if (isNaN(teamsData.teams[matchingTeamIndex].points)) {
@@ -60,6 +31,7 @@ for (const f of raceResultFiles) {
                 teamsData.teams[matchingTeamIndex].results = [];
             }
             teamsData.teams[matchingTeamIndex].results.push({
+                event_name: raceResultData.name,
                 driver: driverName,
                 points: points,
                 position: index + 1
@@ -75,6 +47,7 @@ for (const f of raceResultFiles) {
                 teamsData.teams[matchingTeamIndex].drivers[driverIndex].results = [];
             }
             teamsData.teams[matchingTeamIndex].drivers[driverIndex].results.push({
+                event_name: raceResultData.name,
                 points: points,
                 position: index + 1
             });
