3c07fc42ef496eab7b87a30726a5733db465c67f
devnull-twitch <devnull-twitch@devnull-twitch>
2023-02-20 20:06:06 +0100

    feat: add driver results in details

--- src/DriverRow.tsx
+++ src/DriverRow.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react';
 import { Avatar, Grid, Stack, TableCell, TableRow, Typography } from "@mui/material";
 import { red } from '@mui/material/colors';
-import { Driver as IDriver, Team as ITeam } from './data/interfaces';
+import { Driver as IDriver, Team as ITeam, Result as IResult } from './data/interfaces';
 
 interface DriverRowProps {
   pos: number;
@@ -13,6 +13,10 @@ export const DriverRow: FC<DriverRowProps> = ({ pos, driver, team }) => {
   const [showDetails, setShowDetails] = useState<boolean>(false);
 
   const onTeamRowSelect = () => setShowDetails(!showDetails);
+
+  const filterDriverAndPoints = (r: IResult): boolean => {
+    return r.driver_name === driver.name && (r.points || 0) > 0;
+  }
 
   return (
     <>
@@ -37,6 +41,12 @@ export const DriverRow: FC<DriverRowProps> = ({ pos, driver, team }) => {
           <Typography variant="primaryname">{driver.points || 0} points ( pre-season {driver.pre_season_points || 0} )</Typography>
         </TableCell>
       </TableRow>
+      {showDetails && team !== undefined && team.results && team.results.filter(filterDriverAndPoints).map((r) => (
+        <TableRow>
+          <TableCell colSpan={2}>{r.event_name}</TableCell>
+          <TableCell colSpan={2}>{r.driver_name} Scored {r.points} points coming in at position {r.position}</TableCell>
+        </TableRow>
+      ))}
     </>
   );
 };
